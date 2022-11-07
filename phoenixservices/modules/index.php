e614c2f3d179d52b23a99eda9ff3131ac5d8b151
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-07 11:18:54 -0700

    Se crea archivos de about, index diseño terminado

---
+++ phoenixservices/modules/index.php
@@ -0,0 +1,332 @@
+<!-- start of wpo-hero-section-1 -->
+<section class="hero hero-slider-wrapper hero-style-1">
+    <div class="hero-slider">
+        <div class="slide">
+            <div class="slide-bg">
+                <img src="assets/images/slider/1.jpg" alt="">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-7 col-md-8 col-sm-12 slide-caption">
+                        <div class="hero-slider-content">
+                            <div class="slide-title">
+                                <h2>Get awesome
+                                    property from
+                                    avadh.</h2>
+                            </div>
+                            <div class="slide-subtitle">
+                                <p>It seems that only fragments the original text remain the texts
+                                    also explain can now find slightly versions.</p>
+                            </div>
+                            <div class="btns">
+                                <a href="project-single.html" class="theme-btn">FIND PROJECT<i class="fi "></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero-right-text">
+                        <div class="grid">
+                            <div class="info">
+                                <h3>
+                                    58
+                                </h3>
+                            </div>
+                            <div class="text">
+                                <h4>Years
+                                    Experience</h4>
+                                <span>It seems that only fragments
+                                    original text remain.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slide">
+            <div class="slide-bg">
+                <img src="assets/images/slider/3.jpg" alt="">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-7 col-md-8 col-sm-12 slide-caption">
+                        <div class="hero-slider-content">
+                            <div class="slide-title">
+                                <h2>Balkon Architecture Studio</h2>
+                            </div>
+                            <div class="slide-subtitle">
+                                <p>It seems that only fragments the original text remain the texts
+                                    also explain can now find slightly versions.</p>
+                            </div>
+                            <div class="btns">
+                                <a href="project-single.html" class="theme-btn">FIND PROJECT</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero-right-text">
+                        <div class="grid">
+                            <div class="info">
+                                <h3>
+                                    58
+                                </h3>
+                            </div>
+                            <div class="text">
+                                <h4>Years
+                                    Experience</h4>
+                                <span>It seems that only fragments
+                                    original text remain.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slide">
+            <div class="slide-bg">
+                <img src="assets/images/slider/2.jpg" alt="">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-7 col-md-8 col-sm-12 slide-caption">
+                        <div class="hero-slider-content">
+                            <div class="slide-title">
+                                <h2>Creating Your Future With Affection</h2>
+                            </div>
+                            <div class="slide-subtitle">
+                                <p>It seems that only fragments the original text remain the texts
+                                    also explain can now find slightly versions.</p>
+                            </div>
+                            <div class="btns">
+                                <a href="project-single.html" class="theme-btn">FIND PROJECT</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero-right-text">
+                        <div class="grid">
+                            <div class="info">
+                                <h3>
+                                    58
+                                </h3>
+                            </div>
+                            <div class="text">
+                                <h4>Years
+                                    Experience</h4>
+                                <span>It seems that only fragments
+                                    original text remain.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end of wpo-hero-section-1 slider -->
+
+<!--start wpo-about-section -->
+<section class="wpo-about-section section-padding">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-12">
+                <div class="about-left">
+                    <img src="assets/images/about.jpg" alt="">
+                    <div class="about-left-text">
+                        <img src="assets/images/about-left-shape.png" alt="">
+                        <h5>SINCE</h5>
+                        <span>1998</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12">
+                <div class="about-right">
+                    <div class="text">
+                        <span>MISSION AND VISSION</span>
+                        <h2>Break out of your routine with
+                            a global perspective.</h2>
+                        <p>It seems that only fragments the original of remain texts
+                            used today specul that over the course time certain letters
+                            were various positions.
+                        </p>
+                        <ul>
+                            <li>Where To Invest In Real Estate.</li>
+                            <li>7 Pieces Of Advice To Newbies.</li>
+                            <li>OCutting Your Losses In Real Estate.</li>
+                            <li>50% Off All Organic Product</li>
+                        </ul>
+                        <a href="about.html" class="theme-btn">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end wpo-about-section -->
+
+<!-- start wpo-service-section -->
+<section class="wpo-service-section">
+    <div class="shape-1">
+        <img src="assets/images/scervice-shap-1.png" alt="">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-12">
+                <div class="wpo-section-title">
+                    <span>WHY CHOOSE US?</span>
+                    <h2>Get All Facilities In One Station</h2>
+                    <div class="shape-title">
+                        <img src="assets/images/section-title-shape.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scervice-wrap">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="scervice-item">
+                        <div class="scervice-item-img">
+                            <img src="assets/images/services/icon-1.png" alt="">
+                        </div>
+                        <div class="scervice-item-text">
+                            <a href="service-single.html">
+                                <h2>Floor Plans</h2>
+                            </a>
+                            <p>It seem that only fragmen
+                                the original remain.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="scervice-item">
+                        <div class="scervice-item-img">
+                            <img src="assets/images/services/icon-2.png" alt="">
+                        </div>
+                        <div class="scervice-item-text">
+                            <a href="service-single.html">
+                                <h2>FURNITURE</h2>
+                            </a>
+                            <p>It seem that only fragmen
+                                the original remain.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="scervice-item">
+                        <div class="scervice-item-img">
+                            <img src="assets/images/services/icon-3.png" alt="">
+                        </div>
+                        <div class="scervice-item-text">
+                            <a href="service-single.html">
+                                <h2>PLANNING</h2>
+                            </a>
+                            <p>It seem that only fragmen
+                                the original remain.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="scervice-item">
+                        <div class="scervice-item-img">
+                            <img src="assets/images/services/icon-4.png" alt="">
+                        </div>
+                        <div class="scervice-item-text">
+                            <a href="service-single.html">
+                                <h2>Decor Plan</h2>
+                            </a>
+                            <p>It seem that only fragmen
+                                the original remain.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end wpo-service-section -->
+
+<!-- start wpo-cta-section -->
+<section class="wpo-cta-section">
+    <div class="container">
+        <div class="cta-wrapper">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-7 col-12">
+                    <div class="cta-left">
+                        <h2>Make your dream property.</h2>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-5 col-12">
+                    <div class="cta-right">
+                        <a href="about.html" class="theme-btn">GET IN TOUCH</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end wpo-cta-section -->
+
+
+<!-- start wpo-testimonial-section -->
+<div class="wpo-testimonial-section section-padding" style="margin-top: 1rem;">
+    <div class="container">
+        <div class="testimonial-wrapper">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="testimonial-slider">
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Tanneis Akrasenko</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Funky Dos</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>RockinIt</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>David Gray</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Tanneis Akrasenko</h3>
+                            <span>Business Man</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="testimonial-img">
+                        <img src="assets/images/testimonial.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- end wpo-testimonial-section -->
