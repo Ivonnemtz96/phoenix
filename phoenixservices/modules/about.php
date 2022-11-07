ef9568acff72bb2aead1e641c9826ef0442f8d6e
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-07 14:15:20 -0700

    about ok

--- phoenixservices/modules/about.php
+++ phoenixservices/modules/about.php
@@ -4,10 +4,10 @@
             <div class="row">
                 <div class="col col-xs-12">
                     <div class="wpo-breadcumb-wrap">
-                        <h2>About us</h2>
+                        <h2>Sobre Phoenix Legal Services</h2>
                         <ol class="wpo-breadcumb-wrap">
-                            <li><a href="index.html">Home</a></li>
-                            <li>About</li>
+                            <li><a href="index.php">Inicio</a></li>
+                            <li>Nosotros</li>
                         </ol>
                     </div>
                 </div>
@@ -25,28 +25,26 @@
                         <img src="assets/images/about.jpg" alt="">
                         <div class="about-left-text">
                             <img src="assets/images/about-left-shape.png" alt="">
-                            <h5>SINCE</h5>
-                            <span>1998</span>
+                            <h5>Desde</h5>
+                            <span>2016</span>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-12">
                     <div class="about-right">
                         <div class="text">
-                            <span>MISSION AND VISSION</span>
-                            <h2>Break out of your routine with
-                                a global perspective.</h2>
-                            <p>It seems that only fragments the original of remain texts
-                                used today specul that over the course time certain letters
-                                were various positions.
+                            <span>¿Quiénes somos?</span>
+                            <h2>Phoenix Legal Services</h2>
+                            <p>
+                                Combinamos todos los recurss, conocimeintos y los años de experiencia en el mercado con
+                                nuestros profesionales de talla internacional.
+                                Nuestro objetivo es proporcionar a nuestros clientes un cierre sin problemas y en
+                                tiempo, donde Phoenix Legal Services actúa como el principal coordinador de todas las
+                                partes involucradas en el proceso de cierre, desde que la oferta es aceptada por el
+                                vendedor hasta que el comprador obtiene un título/fideicomiso registrado brindando la
+                                confianza y seguridad que nuestros clientes necesitan guíandolos en todo el proceso.
                             </p>
-                            <ul>
-                                <li>Where To Invest In Real Estate.</li>
-                                <li>7 Pieces Of Advice To Newbies.</li>
-                                <li>OCutting Your Losses In Real Estate.</li>
-                                <li>50% Off All Organic Product</li>
-                            </ul>
-                            <a href="about.html" class="theme-btn">READ MORE</a>
+                            <a href="about.php" class="theme-btn">Ver más</a>
                         </div>
                     </div>
                 </div>
@@ -62,8 +60,9 @@
             <div class="row justify-content-center">
                 <div class="col-lg-6 col-12">
                     <div class="wpo-section-title">
-                        <span>OUR TALENT MEMBER</span>
-                        <h2>Meet Our Experienced Team</h2>
+                        <h2>Conoce a nuestro equipo</h2>
+                        <span>Nos dedicamos a la experiencia de nuestros clientes, ofreciendo sólo las mejores
+                            soluciones para cada caso.</span>
                         <div class="shape-title">
                             <img src="assets/images/section-title-shape-2.png" alt="">
                         </div>
@@ -81,7 +80,11 @@
                                 <a href="team-single.html">
                                     <h2>Henry Bannet</h2>
                                 </a>
-                                <span>Creative Director</span>
+                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvasExample"
+                                    role="button" aria-controls="offcanvasExample" style="border:solid 1px;">
+                                    Ver más
+                                </a>
+
                             </div>
                         </div>
                     </div>
@@ -94,9 +97,13 @@
                                 <a href="team-single.html">
                                     <h2>Tanean Akrasen</h2>
                                 </a>
-                                <span>HEAD OF PLANNING</span>
+                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas2Example"
+                                    role="button" aria-controls="offcanvas2Example" style="border:solid 1px;">
+                                    Ver más
+                                </a>
                             </div>
                         </div>
+
                     </div>
                     <div class="col-lg-3 col-12">
                         <div class="team-single">
@@ -107,9 +114,13 @@
                                 <a href="team-single.html">
                                     <h2>Lily Margerat</h2>
                                 </a>
-                                <span>Art Director</span>
+                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas3Example"
+                                    role="button" aria-controls="offcanvas3Example" style="border:solid 1px;">
+                                    Ver más
+                                </a>
                             </div>
                         </div>
+
                     </div>
                     <div class="col-lg-3 col-12">
                         <div class="team-single">
@@ -120,41 +131,109 @@
                                 <a href="team-single.html">
                                     <h2>William Watson</h2>
                                 </a>
-                                <span>Creative Director</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-12">
-                        <div class="team-single">
-                            <div class="team-single-img">
-                                <img src="assets/images/team/3.jpg" alt="">
-                            </div>
-                            <div class="team-single-text">
-                                <a href="team-single.html">
-                                    <h2>Lady Gaga</h2>
+                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas4Example"
+                                    role="button" aria-controls="offcanvas4Example" style="border:solid 1px;">
+                                    Ver más
                                 </a>
-                                <span>HEAD OF PLANNING</span>
                             </div>
                         </div>
                     </div>
+
                 </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- Menú lateral de los agentes -->
+    <section class="info-agentes">
+        <!-- Info 1 -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample"
+            aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 1</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body text-center">
+                <img src="assets/images/team/1.jpg" alt="">
+                <p style="color: #414141; text-align: justify;margin-top:1rem;">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
+                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
+                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
+                    aliquam, amet atque!
+                </p>
+            </div>
+        </div>
+        <!-- Info 2 -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas2Example"
+            aria-labelledby="offcanvas2ExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 2</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas2"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body text-center">
+                <img src="assets/images/team/1.jpg" alt="">
+                <p style="color: #414141; text-align: justify;margin-top:1rem;">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
+                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
+                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
+                    aliquam, amet atque!
+                </p>
+            </div>
+        </div>
+        <!-- Info 3 -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas3Example"
+            aria-labelledby="offcanvas3ExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 3</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas3"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body text-center">
+                <img src="assets/images/team/1.jpg" alt="">
+                <p style="color: #414141; text-align: justify;margin-top:1rem;">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
+                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
+                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
+                    aliquam, amet atque!
+                </p>
+            </div>
+        </div>
+        <!-- Info 4 -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas4Example"
+            aria-labelledby="offcanvas4ExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 4</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas4"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body text-center">
+                <img src="assets/images/team/1.jpg" alt="">
+                <p style="color: #414141; text-align: justify;margin-top:1rem;">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
+                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
+                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
+                    aliquam, amet atque!
+                </p>
             </div>
         </div>
     </section>
 
-        <!-- start wpo-cta-section -->
-        <section class="wpo-cta-section">
+    <!-- start wpo-cta-section -->
+    <section class="wpo-cta-section">
         <div class="container">
             <div class="cta-wrapper">
                 <div class="row align-items-center">
                     <div class="col-lg-6 col-md-7 col-12">
                         <div class="cta-left">
-                            <h2>Make your dream property.</h2>
+                            <h2>La propiedad de sus sueños.</h2>
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-5 col-12">
                         <div class="cta-right">
-                            <a href="about.html" class="theme-btn">GET IN TOUCH</a>
+                            <a href="tel:1234567890" class="theme-btn">Llamar</a>
                         </div>
                     </div>
                 </div>
