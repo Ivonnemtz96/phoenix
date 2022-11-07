67ed5317c30b4937dc5959e982caa4f5c97b7b2b
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-07 11:54:15 -0700

    Services diseño Ok

---
+++ phoenixservices/modules/services.php
@@ -0,0 +1,111 @@
+<!-- start wpo-page-title -->
+<section class="wpo-page-title">
+    <div class="container">
+        <div class="row">
+            <div class="col col-xs-12">
+                <div class="wpo-breadcumb-wrap">
+                    <h2>Project</h2>
+                    <ol class="wpo-breadcumb-wrap">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Project</li>
+                    </ol>
+                </div>
+            </div>
+        </div> <!-- end row -->
+    </div> <!-- end container -->
+</section>
+<!-- end page-title -->
+
+<!-- start wpo-project-section -->
+<section class="wpo-project-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-12">
+                <div class="wpo-section-title">
+                    <span>SUCCESSFUL PROJECT</span>
+                    <h2>Check Our Most Recent project</h2>
+                    <div class="shape-title">
+                        <img src="assets/images/section-title-shape-2.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12">
+                <div class="project-right">
+                    <a href="project-1.html" class="theme-btn">ALL PROJECT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="project-wrapper">
+        <div class="container-fluid">
+            <div class="row project-slider">
+                <div class="col-lg-3">
+                    <div class="project-single">
+                        <div class="project-single-img">
+                            <img src="assets/images/project/1.jpg" alt="">
+                        </div>
+                        <div class="project-single-text">
+                            <span>KUALA LUMPUR, MALAYSIA</span>
+                            <a href="project-single.html">
+                                <h2>Eskatania Times Square</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="project-single">
+                        <div class="project-single-img">
+                            <img src="assets/images/project/2.jpg" alt="">
+                        </div>
+                        <div class="project-single-text">
+                            <span>ALBERTA, CANADA</span>
+                            <a href="project-single.html">
+                                <h2>Deska Eskatania Mall</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="project-single">
+                        <div class="project-single-img">
+                            <img src="assets/images/project/3.jpg" alt="">
+                        </div>
+                        <div class="project-single-text">
+                            <span>MANILA, PHILIPPINES</span>
+                            <a href="project-single.html">
+                                <h2>Smartes Nasaria of Asia</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="project-single">
+                        <div class="project-single-img">
+                            <img src="assets/images/project/4.jpg" alt="">
+                        </div>
+                        <div class="project-single-text">
+                            <span>PENNSYLVANIA USA</span>
+                            <a href="project-single.html">
+                                <h2>Katariana Prus Malletaka</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="project-single">
+                        <div class="project-single-img">
+                            <img src="assets/images/project/3.jpg" alt="">
+                        </div>
+                        <div class="project-single-text">
+                            <span>KUALA LUMPUR, MALAYSIA</span>
+                            <a href="project-single.html">
+                                <h2>Eskatania Times Square</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end wpo-project-section -->
